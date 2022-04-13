5e6d50fb3dd9f1c162189f526e7bbfe88d915687
jaganmohan reddy <you@example.com>
2022-04-13 23:30:54 +0530

    yesterday assignment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 
-const route = require('../src/routes/route');
+const route = require('./routes/route')
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://jaganreddy-functionup:ORj2ygJHT7jbS3y8@cluster0.nduth.mongodb.net/jaganreddy-24?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+   
+  })
+  .then( () => console.log("MongoDb is connected"))
+  .catch ( err => console.log(err) )
+
+
 
 app.use('/', route);
 
